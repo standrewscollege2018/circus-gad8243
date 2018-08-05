2b2976dc03100207f97e596f731f0313a0d4d92f
Grace Adam <gad8243@stacmail.net>
2018-08-06 10:07:57 +1200

    Setting up Circus_Tickets class
    Just starting to set up the class and the Tkinter root window

---
+++ Practice_Assessment_ticketing.py
@@ -0,0 +1,13 @@
+from tkinter import *
+#define  class
+class Circus_Tickets:
+
+    def __int__(self, name):
+
+
+
+
+root= Tk()
+root.title("Circus Tickets")
+root.resizable(width=FALSE, height=FALSE)
+root.geometry('500x300')

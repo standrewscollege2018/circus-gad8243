3830452b99779b471004d4ab5d65de21320656fe
Grace Adam <gad8243@stacmail.net>
2018-08-08 14:18:57 +1200

    Still on Option Menu
    Still working on option menu with the names showing up and not coming up with errors such as (show_times is not defined). Nearly ready to start doing the other labels displaying the ticket summary which will change after tickets are purchased and when the availabilty changes

--- Practice_Assessment_ticketing.py
+++ Practice_Assessment_ticketing.py
@@ -8,12 +8,20 @@ class Circus:
         self._avalibilty = availabilty
         self._capacity = capacity
         shows.append(self)
+        show_names.append(name)
+        print(self._name, self._availabilty)
 
 def update_label(): 
   #set the value of show_list
    show_list.set("")
    for p in shows:
       show_list.set(show_list.get() + p._name + "  There Are: " + str(p._avalibilty) + " Tickets Available \n" )
+
+def add():
+    for p in shows:
+        if p._name == selected_shows.get():
+            print(selected_shows.get())
+            number_of_tickets.set(number_of_tickets.get() + qty.get())
 
 
 shows = []
@@ -29,28 +37,32 @@ root.title("Circus Tickets")
 root.resizable(width=FALSE, height=FALSE)
 root.geometry('500x300')
 
-#set up a label to display the cureent showings
+#set up a label to display the current showings
 show_list = StringVar()
 
 title_lbl = Label(root, text="Current Showings", justify=LEFT).grid(row=0)
 shows_lbl = Label(root, textvariable=show_list).grid(row=1)
 
-#set up a option menu
-
+# add optionmenu
 selected_shows = StringVar()
 selected_shows.set(show_times[0])
 
-#three parameters: Location, selected item, list of all items
-ticket_menu = OptionMenu(root, selected_shows, *show_times)
-ticket_menu.grid(row=1)
+show_menu = OptionMenu(root, selected_shows, *show_times)
+show_menu.grid(row=2)
 
+# entry field for the quantity of the pizza being ordered
+qty = IntVar()
+qty.set(0)
+qty_entry = Entry(root, textvariable=qty).grid(row=3)
 
-pizza_price = StringVar()
-price_lbl = Label(root, textvariable=pizza_price, width=10)
-price_lbl.grid(row=1, column=1)
+# button to add to order
+add_btn = Button(root, text="Add to purchase", command=add).grid(row=4)
 
-select_btn = Button(root, text="Select", command=update_label)
-select_btn.grid(row=1, column=1)
+# cost label
+number_of_tickets = IntVar()
+number_of_tickets.set(0)
+number_of_tickets_lbl = Label(root, textvariable=number_of_tickets, width=10).grid(row=1, column=1)
+
 
 
 update_label()  
